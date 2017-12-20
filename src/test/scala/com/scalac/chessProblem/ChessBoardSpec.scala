88598425b6340a9384e12a17e61a5be520f44a26
Karol Rodzinski <ext-karol.rodzinski@here.com>
2017-12-20 23:42:37 +0100

    Add test cases to list available positions for given layout

--- src/test/scala/com/scalac/chessProblem/ChessBoardSpec.scala
+++ src/test/scala/com/scalac/chessProblem/ChessBoardSpec.scala
@@ -11,20 +11,58 @@ class ChessBoardSpec extends FlatSpec {
 
   behavior of "A chess board"
 
-  it should "yield all positions" in {
-    assert (chessBoard.positionsOnBoard.size == 49)
-  }
-
   it should "create position from given index" in {
     assert (chessBoard.createFromIndex(4) == Position(1,1))
   }
 
-  it should "aaa" in {
-      val i = new chessBoard.instanceOfChessBoardToPlay(Array(
-        OccupiedPosition(King), AttackedPosition, EmptyPosition,
+  it should "list available position with Queen on chess board in left top corner" in {
+      val instanceOfChessBoard = new chessBoard.instanceOfChessBoardToPlay(Array(
+        OccupiedPosition(Queen), AttackedPosition, AttackedPosition,
         AttackedPosition, AttackedPosition, EmptyPosition,
-        EmptyPosition, EmptyPosition, EmptyPosition
+        AttackedPosition, EmptyPosition, AttackedPosition
       ))
-      assert( i.listOfAvailablePositions.toSet == Set(Position(2, 0), Position(2, 1), Position(0, 2), Position(1, 2), Position(2, 2)))
+      assert(instanceOfChessBoard.listOfAvailablePositions.toSet == Set(Position(2, 1), Position(1, 2)))
   }
+
+  it should "list available position with King on chess board in the middle of it" in {
+    val instanceOfChessBoard = new chessBoard.instanceOfChessBoardToPlay(Array(
+      AttackedPosition, AttackedPosition, AttackedPosition,
+      AttackedPosition, OccupiedPosition(King), AttackedPosition,
+      AttackedPosition, AttackedPosition, AttackedPosition
+    ))
+    assert(instanceOfChessBoard.listOfAvailablePositions.toSet == Set())
+  }
+
+  it should "list available position with Rook on chess board in the middle of it" in {
+    val instanceOfChessBoard = new chessBoard.instanceOfChessBoardToPlay(Array(
+      EmptyPosition, AttackedPosition, EmptyPosition,
+      AttackedPosition, OccupiedPosition(Rook), AttackedPosition,
+      EmptyPosition, AttackedPosition, EmptyPosition
+    ))
+    assert(instanceOfChessBoard.listOfAvailablePositions.toSet == Set(Position(2,2), Position(2,0), Position(0,2), Position(0,0)))
+  }
+
+  it should "list available position with Bishop on chess board in the right top corner" in {
+    val instanceOfChessBoard = new chessBoard.instanceOfChessBoardToPlay(Array(
+      EmptyPosition, EmptyPosition, OccupiedPosition(Bishop),
+      EmptyPosition, AttackedPosition, EmptyPosition,
+      EmptyPosition, EmptyPosition, EmptyPosition
+    ))
+    assert(instanceOfChessBoard.listOfAvailablePositions.toSet == Set(Position(0,2), Position(0,0), Position(2,2), Position(0,1), Position(1,2), Position(2,1), Position(1,0)))
+  }
+
+  it should "list available position with Knight on chess board in the right top corner" in {
+    val instanceOfChessBoard = new chessBoard.instanceOfChessBoardToPlay(Array(
+      EmptyPosition, EmptyPosition, OccupiedPosition(Knight),
+      AttackedPosition, EmptyPosition, EmptyPosition,
+      EmptyPosition, AttackedPosition, EmptyPosition
+    ))
+    assert(instanceOfChessBoard.listOfAvailablePositions.toSet == Set(Position(0,2), Position(0,0), Position(1,1), Position(2,2), Position(2,1), Position(1,0)))
+  }
+
+
+
+
+
+
 }
