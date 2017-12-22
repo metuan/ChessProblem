28675ac3e9a64fdc9749f52ea80f1c3d3f998acc
Karol Rodzinski <ext-karol.rodzinski@here.com>
2017-12-22 02:08:14 +0100

    Run example program, create list of pieces from tuples - it is easy to change to let program read from command line

--- src/main/scala/com/scalac/chessProblem/Runner.scala
+++ src/main/scala/com/scalac/chessProblem/Runner.scala
@@ -1,14 +1,32 @@
 package com.scalac.chessProblem
 
-object Runner extends App{
-  import System.{currentTimeMillis => clockStartStop}
+import com.scalac.chessProblem.model._
 
-  val begin = clockStartStop
-  val numberOfSolutions = 0
-  Thread.sleep(1000)
-  val end = clockStartStop
-  val executionTimeInMilliseconds = (end - begin) / 1000.0
+object Runner {
 
-  println("Number of solutions: " + numberOfSolutions)
-  println("Elapsed time: " + executionTimeInMilliseconds + " ms")
+  def exampleRun() = {
+
+    def setNumberOfPiece(tuple2: Tuple2[Piece, Int]) = {
+      var list : List[Piece] = List()
+      for (i <- 1 to tuple2._2) {
+        list ::= tuple2._1
+      }
+      list
+    }
+
+    val board = new ChessBoard(7,7)
+    val setupOfPieces = List(setNumberOfPiece((King,2)), setNumberOfPiece(Queen, 2), setNumberOfPiece(Bishop, 2), setNumberOfPiece(Knight, 1)).flatten
+    import System.{currentTimeMillis => clockStartStop}
+    val begin = clockStartStop()
+    val (number, list) = board.solve(setupOfPieces)
+    val end = clockStartStop()
+    val dur = (end - begin) / 1000.0
+    list.take(3).foreach(e => println(e + "\n"))
+    println("Number of solutions: " + number)
+    println("\n" + "Elapsed time: "+ dur +" ms")
+  }
+
+  def main(args: Array[String]): Unit = {
+    exampleRun()
+  }
 }
